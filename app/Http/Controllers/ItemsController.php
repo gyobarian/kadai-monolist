3e9fc54c2bfb78a32b0ee96c19dde56b2faebfba
Shusuke Gyoba <s.gyoba.com@gmail.com>
2017-03-19 16:36:35 +0000

    rakuten api

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class WelcomeController extends Controller
+class ItemsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,7 +16,7 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        //
     }
 
     /**
@@ -26,7 +26,33 @@ class WelcomeController extends Controller
      */
     public function create()
     {
-        //
+        $keyword = request()->keyword;
+        $items = [];
+        if ($keyword) {
+            $client = new \RakutenRws_Client();
+            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+
+            $rws_response = $client->execute('IchibaItemSearch', [
+                'keyword' => $keyword,
+                'imageFlag' => 1,
+                'hits' => 20,
+            ]);
+
+            // 扱い易いように Item としてインスタンスを作成する（保存はしない）
+            foreach ($rws_response->getData()['Items'] as $rws_item) {
+                $item = new Item();
+                $item->code = $rws_item['Item']['itemCode'];
+                $item->name = $rws_item['Item']['itemName'];
+                $item->url = $rws_item['Item']['itemUrl'];
+                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
+                $items[] = $item;
+            }
+        }
+
+        return view('items.create', [
+            'keyword' => $keyword,
+            'items' => $items,
+        ]);
     }
 
     /**
